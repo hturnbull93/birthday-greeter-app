c7545373b61579dcfb0bcc3f5ece0f1a04306100
Harry Turnbull <hturnbull@live.co.uk>
2020-03-31 22:22:16 +0100

    added post for home

--- birthday_app.rb
+++ birthday_app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'date'
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  @name = params[:name]
+  @birthday = Date.parse("#{params[:month]} #{params[:day]}")
+  @today = Date.today
+  if @today == @birthday
+    @result = "Happy Birthday #{@name}! 🥳"
+  else
+    @birthday += 365 if @birthday < @today
+    @days_until_birthday = (@birthday - @today).to_i
+    @day_wording = @days_until_birthday == 1 ? "day" : "days"
+    @result = "Your birthday will be in #{@days_until_birthday} #{@day_wording}, #{@name}. 🕓"
+  end
+
+  erb :result
 end
