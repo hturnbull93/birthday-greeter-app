9f57cb16c8c875ba61163b46db573b0638f11406
Harry Turnbull <hturnbull@live.co.uk>
2020-03-31 23:08:19 +0100

    refactored emojis to allow wiggling

--- birthday_app.rb
+++ birthday_app.rb
@@ -10,12 +10,14 @@ post '/' do
   @birthday = Date.parse("#{params[:month]} #{params[:day]}")
   @today = Date.today
   if @today == @birthday
-    @result = "Happy Birthday #{@name}! 🥳"
+    @result = "Happy Birthday #{@name}!"
+    @wiggler = '🥳'
   else
     @birthday += 365 if @birthday < @today
     @days_until_birthday = (@birthday - @today).to_i
     @day_wording = @days_until_birthday == 1 ? "day" : "days"
-    @result = "Your birthday will be in #{@days_until_birthday} #{@day_wording}, #{@name}. 🕓"
+    @result = "Your birthday will be in #{@days_until_birthday} #{@day_wording}, #{@name}."
+    @wiggler = '🕓'
   end
 
   erb :result
